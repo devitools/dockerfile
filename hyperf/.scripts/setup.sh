935cf44fc7ad1d6d87222e68007728d9d9e52797
William Correa <wilcorrea@gmail.com>
2025-02-24 23:04:27 -0300

    feat: add hyperf support

---
+++ hyperf/.scripts/setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+TIMEZONE=${1:-UTC}
+
+git config --global --add safe.directory /opt/www
+
+# - config PHP
+cd /etc/php83
+{
+    echo "upload_max_filesize=128M"
+    echo "post_max_size=128M"
+    echo "memory_limit=1G"
+    echo "date.timezone=${TIMEZONE}"
+} | tee conf.d/99_overrides.ini
+
+# - config timezone
+ln -sf "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
+echo "${TIMEZONE}" > /etc/timezone
